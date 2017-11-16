f429db214c344c12e10c2a75988592380961870b
Kyle Atchison <kylea@HSC.prv>
2017-11-16 14:44:51 -0600

    temp fix

--- models/tech.js
+++ models/tech.js
@@ -1,20 +1,22 @@
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
     const Tech = sequelize.define("Tech", {
         name: DataTypes.STRING,
         email: DataTypes.STRING,
         password: DataTypes.STRING,
-    }, {
-            instanceMethods: {
-                generateHash(password) {
-                    return bcrypt.hash(password, bcrypt.genSaltSync(5));
-                },
-                validpassword(password) {
-                    return bcrypt.compare(password, this.password);
-                }
-            }
-        });
+    }, 
+    // {
+    //         instanceMethods: {
+    //             generateHash(password) {
+    //                 return bcrypt.hash(password, bcrypt.genSaltSync(5));
+    //             },
+    //             validpassword(password) {
+    //                 return bcrypt.compare(password, this.password);
+    //             }
+    //         }
+    //     }
+    );
 
     return Tech;
 };
