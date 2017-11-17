d6192b3fb7bcc10f8a6da04c320c37cd08d8cc3a
Kyle Atchison <kylea@HSC.prv>
2017-11-17 12:09:10 -0600

    more changes to routing

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const customers = require('./customers');
 const customerInfos = require('./customerInfos');
-
+const techs = require("./techs");
 module.exports = {
     customers,
     customerInfos,
+    techs
 };
