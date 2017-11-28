b2ae3dc8551752441068428ecd64de196d13ef6e
Kyle Atchison <katchison@gmail.com>
2017-11-27 20:12:57 -0600

    testing signature

--- client/src/pages/Complete/Complete.js
+++ client/src/pages/Complete/Complete.js
@@ -1,11 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+var SignaturePad = require('react-signature-pad');
 
-const App = () => (
-    <div>
-        done
-        <Link to={`/`} className="btn btn-lg loginBtn btn-outline-primary">Home</Link>
-</div>
-);
+class App extends Component {
+
+    render() {
+        return (
+
+            <div>
+                done
+                <SignaturePad
+                clearButton = "true"
+                ref="signature"
+                 />
+                <Link to={`/`} className="btn btn-lg loginBtn btn-outline-primary">Home</Link>
+            </div>
+        );
+    };
+};
 
 export default App;
